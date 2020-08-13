3e5ac415d8f0508ff0237243498493bc0afbc770
mj <mj@pop-os.localdomain>
2020-08-14 00:20:42 +0800

    mongo connection

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
 require('dotenv').config();
+
+const Food = require('./models/foodSchema');
 
 app.use(morgan('tiny'));
 
@@ -10,6 +13,51 @@ app.get('/', (req, res) => {
   res.json({ message: 'hello from deglem' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log('listening on port:', process.env.PORT);
-});
+
+
+
+
+
+// connect to mongo db
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0-bojs0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('listening on port:', process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
+
+// const potato = new Food({
+//   name: 'Potato',
+//   serving: {
+//     size: 1,
+//     unit: 'medium',
+//     servingPerContainer: 1
+//   },
+//   calories: 110,
+//   totalFat: 0.2,
+//   saturatedFat: 0,
+//   transFat: 0,
+//   polyUnsaturatedFat: 0,
+//   monoUnsaturatedFat: 0,
+//   cholestrol: 0,
+//   sodium: 0,
+//   totalCarbohydrates: 26,
+//   dietaryFibers: 2,
+//   sugars: 2,
+//   addedSugars: 0,
+//   sugarAlchohols: 0,
+//   protein: 3,
+//   vitaminD: 0,
+//   calcium: 2,
+//   iron: 6,
+//   potassium: 620,
+//   vitaminA: 0,
+//   vitaminC: 45
+// });
+
+// potato.save();
---
+++ models/foodSchema.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const foodSchema = new mongoose.Schema({
+  barcode: {
+    type: Schema.Types.Number
+  },
+  name: {
+    type: Schema.Types.String,
+    required: true
+  },
+  serving: {
+      size: {
+          type: Schema.Types.Number,
+          required: true
+      },
+      unit: {
+          type: Schema.Types.String,
+          required: true
+      },
+      servingPerContainer: {
+          type: Schema.Types.Number,
+          required: true
+      }
+  },
+  calories: {
+      type: Schema.Types.Number,
+      required: true,
+  },
+  totalFat: {
+    type: Schema.Types.Number
+  },
+  saturatedFat: {
+    type: Schema.Types.Number
+  },
+  transFat: {
+    type: Schema.Types.Number
+  },
+  polyUnsaturatedFat: {
+    type: Schema.Types.Number
+  },
+  monoUnsaturatedFat: {
+    type: Schema.Types.Number
+  },
+  cholestrol: {
+    type: Schema.Types.Number
+  },
+  sodium: {
+    type: Schema.Types.Number
+  },
+  totalCarbohydrates: {
+    type: Schema.Types.Number
+  },
+  dietaryFibers: {
+    type: Schema.Types.Number
+  },
+  sugars: {
+    type: Schema.Types.Number
+  },
+  addedSugars: {
+    type: Schema.Types.Number
+  },
+  sugarAlchohols: {
+    type: Schema.Types.Number
+  },
+  protein: {
+    type: Schema.Types.Number
+  },
+  vitaminD: {
+    type: Schema.Types.Number
+  },
+  calcium: {
+    type: Schema.Types.Number
+  },
+  iron: {
+    type: Schema.Types.Number
+  },
+  potassium: {
+    type: Schema.Types.Number
+  },
+  vitaminA: {
+    type: Schema.Types.Number
+  },
+  vitaminC: {
+    type: Schema.Types.Number
+  }
+});
+
+module.exports = mongoose.model('Food', foodSchema)
