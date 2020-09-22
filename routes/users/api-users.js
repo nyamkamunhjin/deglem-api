f2397451adc23ebd47cab4a09d368531c183083c
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2020-09-22 21:45:49 +0800

    api fixes

--- routes/users/api-users.js
+++ routes/users/api-users.js
@@ -13,17 +13,47 @@ router.get('/dailylog', async (req, res) => {
     if (!id) throw new Error('id query is null');
 
     // find user by id and populate daily log
-    const user = await User.findOne({ _id: id })
-      .populate({
-        path: 'dailyLog',
+    const user = await User.findOne({ _id: '5f607f85a586e00e416f2124' })
+    .populate({
+      path: 'dailyLog',
+      populate: {
+        path: 'breakfast',
         populate: {
-          path: 'breakfast',
-          populate: {
-            path: 'food',
-            model: 'Food',
-          },
+          path: 'food',
+          model: 'Food',
         },
-      })
+      },
+    })
+    .populate({
+      path: 'dailyLog',
+      populate: {
+        path: 'lunch',
+        populate: {
+          path: 'food',
+          model: 'Food',
+        },
+      },
+    })
+    .populate({
+      path: 'dailyLog',
+      populate: {
+        path: 'dinner',
+        populate: {
+          path: 'food',
+          model: 'Food',
+        },
+      },
+    })
+    .populate({
+      path: 'dailyLog',
+      populate: {
+        path: 'snacks',
+        populate: {
+          path: 'food',
+          model: 'Food',
+        },
+      },
+    })
       .exec();
     if (user.dailyLog !== undefined) res.status(200).json(user.dailyLog);
     // send error
