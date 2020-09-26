1371bd0d4fea0d5c11bc233e2295264184ad9dbe
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2020-09-26 21:10:13 +0800

    update food, delete food endpoints added

--- models/diarySchema.js
+++ models/diarySchema.js
@@ -2,27 +2,17 @@ const mongoose = require('mongoose');
 const User = require('./userSchema');
 const Schema = mongoose.Schema;
 
-const singleFoodTypeSchema = Schema(
-  {
-    serving: {
-      type: Schema.Types.Number,
-      required: true,
-    },
-    food: {
-      type: Schema.Types.ObjectId,
-      ref: 'Food',
-      required: true,
-    },
+const singleFoodTypeSchema = new Schema({
+  serving: {
+    type: Schema.Types.Number,
+    required: true,
   },
-  { _id: false }
-);
-// const diarySchema = new Schema({
-//   diaryDate: [Schema.Types.Date],
-//   breakfast: [{type:Schema.Types.ObjectId, ref: 'FoodLog'}],
-//   lunch: [{type:Schema.Types.ObjectId, ref: 'FoodLog'}],
-//   dinner: [{type:Schema.Types.ObjectId, ref: 'FoodLog'}],
-//   snacks: [{type:Schema.Types.ObjectId, ref: 'FoodLog'}]
-// })
+  food: {
+    type: Schema.Types.ObjectId,
+    ref: 'Food',
+    required: true,
+  },
+});
 
 const diarySchema = new Schema({
   date: Schema.Types.String,
@@ -31,6 +21,7 @@ const diarySchema = new Schema({
     ref: 'User',
     required: true,
     validate: {
+      // validate user with user_id actually exists
       validator: (value) =>
         User.findOne({ _id: value }, (err, doc) => {
           if (err || !doc) return false;
@@ -64,9 +55,24 @@ diarySchema.post('findOneAndUpdate', async (doc) => {
   }
 });
 
-// validate user with user_id actually exists
+diarySchema.post('findOneAndDelete', async (doc) => {
+  console.log('post findOneAndDelete hook: ', doc);
+  // const user = await User.findById({ _id: doc.user_id });
+  // if (!user.dailyLog.includes(doc._id)) {
+  //   user.dailyLog.push(doc._id);
+  //   user.save((err, doc) => {
+  //     if (err || !doc) console.log(err);
+  //     else {
+  //       console.log('linked diary doc to user.');
+  //     }
+  //   });
+  // } else {
+  //   console.log('user already linked.');
+  // }
+});
 
 module.exports = {
   Diary: mongoose.model('Diary', diarySchema),
+  SingleFood: mongoose.model('SingleFood', singleFoodTypeSchema),
   diarySchema,
 };
--- routes/users/api-users.js
+++ routes/users/api-users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Types } = require('mongoose');
 const { formatDate } = require('../../functions/functions');
-const { Diary } = require('../../models/diarySchema');
+const { Diary, SingleFood } = require('../../models/diarySchema');
 const userSchema = require('../../models/userSchema');
 const User = require('../../models/userSchema');
 
@@ -10,6 +10,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/dailylog', async (req, res) => {
+  /*
+  GET ALL DIARIES OF A USER
+
+  */
   try {
     const { id } = req.query;
 
@@ -67,8 +71,9 @@ router.get('/dailylog', async (req, res) => {
   }
 });
 
-router.post('/dailylog/add', async (req, res) => {
+router.post('/dailylog/food/add', async (req, res) => {
   /* 
+  INSERT NEW FOOD
   1. find user by id 
   2. check if the day's diary exists else create new diary 
   3. add the food to a diary by id
@@ -102,4 +107,60 @@ router.post('/dailylog/add', async (req, res) => {
     res.status(500).json({ success: false, message: err.message });
   }
 });
+
+router.put('/dailylog/food/update', async (req, res) => {
+  /*
+  UPDATE FOOD SERVING 
+  */
+  try {
+    console.log(req.body);
+
+    Diary.findOneAndUpdate(
+      req.body.filter,
+      {
+        $set: req.body.update,
+      },
+      (err, doc) => {
+        if (err || !doc) console.log(err);
+        else {
+          console.log(doc);
+          res.status(200).json({
+            success: true,
+            message: 'diary food successfully updated.',
+          });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+router.delete('/dailylog/food/delete', async (req, res) => {
+  /*
+  REMOVE LOGGED FOOD 
+  */
+  try {
+    Diary.updateOne(
+      {
+        $pull: req.body.pull,
+      },
+      (err, doc) => {
+        if (err || !doc) console.log(err);
+        else {
+          console.log(doc);
+          res.status(200).json({
+            success: true,
+            message: 'diary food successfully removed.',
+          });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
 module.exports = router;
